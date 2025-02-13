ffa79d307f0756542f5b19ef2bf32d23bc898894
SrPio <andresfer1799@gmail.com>
2025-02-13 13:14:13 -0500

    Enhances TechnicalSheet and styles image in Popup
    Adds height and object-fit to image in Popup for better display
    Adds SecondaryHeader and id to TechnicalSheet for improved structure

--- src/app/ver-mas/termicos/page.js
+++ src/app/ver-mas/termicos/page.js
@@ -6,11 +6,13 @@ import WppBtn from "@/app/components/WppBtn/WppBtn";
 import Footer from "@/app/components/Footer/Footer";
 import style from "../../rollos-termicos/components/RelatedProducts/RelatedProducts.module.scss";
 import RelatedProducts from "@/app/rollos-termicos/components/RelatedProducts/RelatedProducts";
+import SecondaryHeader from "@/app/components/SecondaryHeader/SecondaryHeader";
 
 export default function VerMasTermicos() {
     return (
         <>
             <TermicosProductGallery />
+            <SecondaryHeader />
             <TechnicalSheet />
             <LogosSection />
             <RelatedProducts titleClassName={style.customTitle} />

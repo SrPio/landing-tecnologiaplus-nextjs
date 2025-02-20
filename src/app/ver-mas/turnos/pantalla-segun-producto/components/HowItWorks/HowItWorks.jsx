a40825492c9bb8d189e1be519a2be25fbbe2f8a7
SrPio <andresfer1799@gmail.com>
2025-02-20 16:13:24 -0500

    Adds HowItWorks, ProductGallery, and TechnicalSheet components
    Introduces new components: HowItWorks, ProductGallery, and TechnicalSheet
    for displaying product information, usage instructions, and technical details.
    Includes associated styles and updates page structure.
    
    Enhances user experience by providing detailed visuals and descriptions.

---
+++ src/app/ver-mas/turnos/pantalla-segun-producto/components/HowItWorks/HowItWorks.jsx
@@ -0,0 +1,49 @@
+import styles from "./HowItWorks.module.scss";
+
+function HowItWorks() {
+  return (
+    <section id="clients-section" className={styles.HowItWorks__container}>
+      <h2>¿Cómo funciona?</h2>
+      <div className={styles.items__container}>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740084322/como_funciona_4x-8_3_mamyu7.webp"
+            alt="imagen de personas tomando su turno"
+          />
+          <p>
+            El cliente recibe el número
+            <br />
+            de turno para su pedido
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740084320/como_funciona_1_4x-8_3_tlakgc.webp"
+            alt="imagen de persona digitando en el control el numero de turno"
+          />
+          <p>
+            Cuando el pedido está listo, digita en
+            <br />
+            el control el número del turno
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740084320/como_funciona_2_4x-8_3_m8bouq.webp"
+            alt="imagen de pantalla mostrando el número de turno"
+          />
+          <p>
+            La pantalla suena
+            <br />
+            avisando al cliente del llamado
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HowItWorks;

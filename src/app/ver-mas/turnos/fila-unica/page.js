b8e21476c77d201e66156c517a3d77a0be46c071
SrPio <andresfer1799@gmail.com>
2025-02-20 09:51:05 -0500

    Updates related products component and technical sheet styles
    Adds optional title class name to TurnoExpressRelatedProducts component
    Updates TechnicalSheet styles to improve image handling
    Fixes incorrect import in VerMasTurnosFilaUnica page

--- src/app/ver-mas/turnos/fila-unica/page.js
+++ src/app/ver-mas/turnos/fila-unica/page.js
@@ -1,13 +1,13 @@
 import ContactForm from "@/app/rollos-termicos/components/ContactForm/ContactForm";
 import WppBtn from "@/app/components/WppBtn/WppBtn";
 import Footer from "@/app/components/Footer/Footer";
-import style from "../../../rollos-termicos/components/RelatedProducts/RelatedProducts.module.scss";
-import RelatedProducts from "@/app/rollos-termicos/components/RelatedProducts/RelatedProducts";
+import style from "../../../turnero-turnoexpress/components/RelatedProducts/TurnoExpressRelatedProducts.module.scss";
 import SecondaryHeader from "@/app/components/SecondaryHeader/SecondaryHeader";
 import TurnosFilaUnicaProductGallery from "./components/ProductGallery/TurnosFilaUnicaProductGallery";
 import LogosSection from "../../termicos/components/LogosSection/LogosSection";
 import TechnicalSheet from "./components/TechnicalSheet/TechnicalSheet";
 import HowItWorks from "./components/HowItWorks/HowItWorks";
+import TurnoExpressRelatedProducts from "@/app/turnero-turnoexpress/components/RelatedProducts/TurnoExpressRelatedProducts";
 
 export default function VerMasTurnosFilaUnica() {
     return (
@@ -17,7 +17,7 @@ export default function VerMasTurnosFilaUnica() {
             <TechnicalSheet />
             <HowItWorks />
             <LogosSection />
-            <RelatedProducts titleClassName={style.customTitle} />
+            <TurnoExpressRelatedProducts titleClassName={style.customTitle} />
             <ContactForm />
             <WppBtn />
             <Footer />

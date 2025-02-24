2f402c626ee4984374e52657d0fb10b1f6eb4e8f
SrPio <andresfer1799@gmail.com>
2025-02-24 16:12:41 -0500

    Adds HowItWorks and TechnicalSheet components
    Introduces HowItWorks and TechnicalSheet components to display functionality and technical specifications
    Includes associated styling and integrates components into the VerMasPantallaT1B page layout

---
+++ src/app/ver-mas/meseros/pantalla-t1b/page.js
@@ -0,0 +1,26 @@
+import ContactForm from "@/app/rollos-termicos/components/ContactForm/ContactForm";
+import WppBtn from "@/app/components/WppBtn/WppBtn";
+import Footer from "@/app/components/Footer/Footer";
+import style from "../../../turnero-turnoexpress/components/RelatedProducts/TurnoExpressRelatedProducts.module.scss";
+import SecondaryHeader from "@/app/components/SecondaryHeader/SecondaryHeader";
+import ProductGallery from "../../turnos/pantalla-t1b/components/ProductGallery/ProductGallery";
+import LogosSection from "../../termicos/components/LogosSection/LogosSection";
+import TechnicalSheet from "./components/TechnicalSheet/TechnicalSheet";
+import HowItWorks from "./components/HowItWorks/HowItWorks";
+import LlamadorMeserosRelatedProducts from "@/app/llamador-meseros/components/RelatedProducts/LlamadorMeserosRelatedProducts";
+
+export default function VerMasPantallaT1B() {
+    return (
+        <>
+            <ProductGallery />
+            <SecondaryHeader />
+            <TechnicalSheet />
+            <HowItWorks />
+            <LogosSection />
+            <LlamadorMeserosRelatedProducts titleClassName={style.customTitle} />
+            <ContactForm />
+            <WppBtn />
+            <Footer />
+        </>
+    )
+}

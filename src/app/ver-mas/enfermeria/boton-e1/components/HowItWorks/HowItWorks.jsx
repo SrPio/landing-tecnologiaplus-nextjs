d9bdd0a50f3ef57260b4e20bc33fcfdcd1db6ab1
SrPio <andresfer1799@gmail.com>
2025-02-25 12:29:39 -0500

    Adds custom title class prop to RelatedProducts component
    Allows passing a custom class name for the title element
    Enhances flexibility in styling the component's title

---
+++ src/app/ver-mas/enfermeria/boton-e1/components/HowItWorks/HowItWorks.jsx
@@ -0,0 +1,45 @@
+import styles from "../../../../styles/HowItWorks.module.scss";
+
+function HowItWorks() {
+  return (
+    <section id="clients-section" className={styles.HowItWorks__container}>
+      <h2>¿Cómo funciona?</h2>
+      <div className={styles.items__container}>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740498991/13_4x-8_uupi68.webp"
+            alt="imagen de paciente presionando botón de llamado"
+          />
+          <p>
+            Presiona el botón para
+            <br />
+            llamar la enfermera
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740498992/11_4x-8_mkfm30.webp"
+            alt="imagen de la enfermera recibiendo llamado"
+          />
+          <p>
+            La enfermera recibe el llamado en la
+            <br />
+            pantalla o en el reloj receptor
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740498991/12_4x-8_y6cste.webp"
+            alt="imagen de la enfermera atendiendo llamado"
+          />
+          <p>La enfermera atiende el llamado</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HowItWorks;

d23f1ff90193a3cbd7e2f605ef9725f6695f638b
SrPio <andresfer1799@gmail.com>
2025-02-27 10:41:25 -0500

    Adds HowItWorks and TechnicalSheet components
    Implements HowItWorks and TechnicalSheet components with corresponding styles and integrates them into VerMasPantallaT1B page.

---
+++ src/app/ver-mas/enfermeria/pantalla-t1b/components/HowItWorks/HowItWorks.jsx
@@ -0,0 +1,49 @@
+import styles from "../../../../styles/HowItWorks.module.scss";
+
+function HowItWorks() {
+  return (
+    <section id="clients-section" className={styles.HowItWorks__container}>
+      <h2>¿Cómo funciona?</h2>
+      <div className={styles.items__container}>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740670192/17_4x-8_1_peyzbj.webp"
+            alt="imagen de paciente presionando el botón de llamado"
+          />
+          <p>
+            Presiona el botón para
+            <br />
+            llamar la enfermera
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740670192/15_4x-8_3_s9x8p9.webp"
+            alt="imagen de pantalla de enfermera recibiendo el llamado"
+          />
+          <p>
+            La enfermera recibe el
+            <br />
+            llamado en la pantalla
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740670192/16_4x-8_1_eielcu.webp"
+            alt="imagen de enfermera atendiendo el llamado"
+          />
+          <p>
+            Se atiende el llamado y presiona la tecla
+            <br />
+            "CANCEL" para borrar el llamado
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HowItWorks;
---
+++ src/app/ver-mas/enfermeria/pantalla-t1b/components/TechnicalSheet/TechnicalSheet.jsx
@@ -0,0 +1,106 @@
+import styles from "./TechnicalSheet.module.scss";
+
+function TechnicalSheet() {
+  return (
+    <section id="clients-section" className={styles.technicalsheet__container}>
+      <h2>Ficha técnica</h2>
+      <div className={styles.items__container}>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740161084/9_4x-8_l5gvik.webp"
+            alt="icono de compatible con equipos de tecnología plus"
+          />
+          <p>
+            Compatible con: Equipos
+            <br />
+            de Tecnología Plus.
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1739998010/icono_7_4x-8_gkqxdl.webp"
+            alt="icono de antena"
+          />
+          <p>
+            Recibe los llamados
+            <br />
+            de forma inalámbrica.
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1739998010/icono_6_4x-8_bmmdoa.webp"
+            alt="icono de enchufe"
+          />
+          <p>Conexión eléctrica.</p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740065557/ICONO_1_4x-8_1_xmpmcr.webp"
+            alt="icono de Tecnología Plus"
+          />
+          <p>
+            Compatible con:
+            <br />
+            Equipos de
+            <br />
+            Tecnología Plus.
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740503168/6_4x-8_jbsk2k.webp"
+            alt="icono de hospital"
+          />
+          <p>
+            Tipo de uso:
+            <br />
+            sector salud y bienestar
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740065557/ICONO_3_4x-8_1_feoyft.webp"
+            alt="icono de pantalla visible"
+          />
+          <p>
+            1 llamado visible
+            <br />
+            en pantalla
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740065558/ICONO_6_4x-8_1_gx8jhi.webp"
+            alt="icono de pantalla de 3 dígitos"
+          />
+          <p>
+            Muestra hasta
+            <br />3 dígitos
+          </p>
+        </div>
+        <div className={styles.item}>
+          <img
+            loading="lazy"
+            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740065557/ICONO_5_4x-8_1_wkvdpj.webp"
+            alt="icono de alerta con sonido y número en pantalla"
+          />
+          <p>
+            Alerta con sonido y
+            <br />
+            número en pantalla
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TechnicalSheet;

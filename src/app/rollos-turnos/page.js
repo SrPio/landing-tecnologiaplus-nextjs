6dfc3778fe78e0493451566bcb4ca7a214a86c87
SrPio <andresfer1799@gmail.com>
2025-03-04 09:57:25 -0500

    Adds metadata and Open Graph tags to RollosTurnosLanding component

--- src/app/rollos-turnos/page.js
+++ src/app/rollos-turnos/page.js
@@ -10,6 +10,45 @@ import TurnosProductsSection from "./components/ProductsSection/TurnosProductsSe
 import TurnosRelatedProducts from "./components/RelatedProducts/TurnosRelatedProducts";
 import TurnosTechnicalDetails from "./components/TechnicalDetails/TurnosTechnicalDetails";
 
+export const metadata = {
+    title: "Rollos de turnos - TecnologiaPlus",
+    description:
+        "Descubre cómo Tecnología Plus optimiza tu negocio con rollos de turnos especializados para una gestión eficiente.",
+    robots: "index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+    alternates: {
+        canonical: "https://tecnologiaplus.com/rollos-de-turno/",
+    },
+    openGraph: {
+        type: "article",
+        locale: "es_ES",
+        url: "https://tecnologiaplus.com/rollos-de-turno/",
+        title: "Rollos de turnos - TecnologiaPlus",
+        description:
+            "Descubre cómo Tecnología Plus optimiza tu negocio con rollos de turnos especializados para una gestión eficiente.",
+        siteName: "TecnologiaPlus",
+        images: [
+            {
+                url: "https://tecnologiaplus.com/wp-content/uploads/2023/06/Nueva-imagen-rollos-de-turnos-2-1-1.png",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Rollos de turnos - TecnologiaPlus",
+        description:
+            "Descubre cómo Tecnología Plus optimiza tu negocio con rollos de turnos especializados para una gestión eficiente.",
+    },
+    icons: {
+        other: [
+            {
+                rel: "msapplication-TileImage",
+                url: "https://tecnologiaplus.com/wp-content/uploads/2018/08/cropped-favicon-tecnologia-plus-270x270.png",
+            },
+        ],
+    },
+};
+
+
 export default function RollosTurnosLanding() {
     return (
         <>

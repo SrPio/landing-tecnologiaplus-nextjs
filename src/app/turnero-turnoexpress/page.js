da17d8712660ca13741ff912a970c05dfb48dc40
SrPio <andresfer1799@gmail.com>
2025-03-04 09:49:45 -0500

    Adds metadata and Open Graph tags to TurnoExpressLanding component

--- src/app/turnero-turnoexpress/page.js
+++ src/app/turnero-turnoexpress/page.js
@@ -10,6 +10,49 @@ import TurnoExpressProductsSection from "./components/ProductsSection/TurnoExpre
 import TurnoExpressRelatedProducts from "./components/RelatedProducts/TurnoExpressRelatedProducts";
 import SecondaryTurnoExpressProductsSection from "./components/SecondaryProductsSection/SecondaryTurnoExpressProductsSection";
 
+export const metadata = {
+    title: "Turnero Digital: Agiliza y Organiza las Filas en tu negocio.",
+    description:
+        "Turnero digital: Somos fabricantes. Acelera la atención al cliente y moderniza tu negocio. ¡Solicita una asesoria ahora!",
+    robots: "index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+    alternates: {
+        canonical: "https://tecnologiaplus.com/turnero/",
+    },
+    openGraph: {
+        locale: "es_ES",
+        type: "article",
+        title: "Turnero Digital: Agiliza y Organiza las Filas en tu negocio.",
+        description:
+            "Turnero digital: Somos fabricantes. Acelera la atención al cliente y moderniza tu negocio. ¡Solicita una asesoria ahora!",
+        url: "https://tecnologiaplus.com/turnero/",
+        siteName: "TecnologiaPlus",
+        images: [
+            {
+                url: "https://tecnologiaplus.com/wp-content/uploads/2020/09/banner-tur-1600px.jpg",
+                width: 1600,
+                height: 600,
+                alt: "pantallas de turnos",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+    },
+    other: {
+        "article:publisher": "https://www.facebook.com/tecnologiapluscolombia",
+        "article:modified_time": "2024-12-16T22:12:14+00:00",
+    },
+    icons: {
+        other: [
+            {
+                rel: "msapplication-TileImage",
+                url: "https://tecnologiaplus.com/wp-content/uploads/2018/08/cropped-favicon-tecnologia-plus-270x270.png",
+            },
+        ],
+    },
+};
+
+
 export default function TurnoExpressLanding() {
     return (
         <>

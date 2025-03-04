0b035e577c8b9042a28097a0acaf88394259430f
SrPio <andresfer1799@gmail.com>
2025-03-04 10:58:32 -0500

    Adds metadata and Open Graph tags to Dispensador de tiquetes page

--- src/app/dispensador-tiquetes/page.js
+++ src/app/dispensador-tiquetes/page.js
@@ -9,6 +9,50 @@ import DispensadorTiquetesIndustriesSection from "./components/IndustriesSection
 import DispensadorTiquetesProductsSection from "./components/ProductsSection/DispensadorTiquetesProductsSection";
 import DispensadorTiquetesRelatedProducts from "./components/RelatedProducts/DispensadorTiquetesRelatedProducts";
 
+
+export const metadata = {
+    title: "Dispensador de tiquetes - TecnologiaPlus",
+    description: "Descubre nuestro dispensador de tiquetes soluciones eficientes para tu negocio. Encuentra variedad y calidad en Tecnología Plus.",
+    robots: "index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+    alternates: {
+        canonical: "https://tecnologiaplus.com/dispensador-de-tiquetes/",
+    },
+    openGraph: {
+        type: "article",
+        locale: "es_ES",
+        url: "https://tecnologiaplus.com/dispensador-de-tiquetes/",
+        title: "Dispensador de tiquetes - TecnologiaPlus",
+        description: "Descubre nuestro dispensador de tiquetes soluciones eficientes para tu negocio. Encuentra variedad y calidad en Tecnología Plus.",
+        siteName: "TecnologiaPlus",
+        images: [
+            {
+                url: "https://tecnologiaplus.com/wp-content/uploads/2023/06/Captura-de-pantalla-2023-05-25-210121.png",
+                width: 958,
+                height: 747,
+                alt: "Dispensador de tiquetes",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Dispensador de tiquetes - TecnologiaPlus",
+        description: "Descubre nuestro dispensador de tiquetes soluciones eficientes para tu negocio. Encuentra variedad y calidad en Tecnología Plus.",
+    },
+    other: {
+        "article:publisher": "https://www.facebook.com/tecnologiapluscolombia",
+        "article:modified_time": "2025-02-27T15:41:02+00:00",
+    },
+    icons: {
+        other: [
+            {
+                rel: "msapplication-TileImage",
+                url: "https://tecnologiaplus.com/wp-content/uploads/2018/08/cropped-favicon-tecnologia-plus-270x270.png",
+            },
+        ],
+    },
+};
+
+
 export default function DispensadorTiquetesLanding() {
     return (
         <>

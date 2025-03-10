dd433090c69c70b7e6eaa746a295583a087aaa16
SrPio <andresfer1799@gmail.com>
2025-03-10 14:17:07 -0500

    Refactors import path for ProductGallery component in pantalla-t1b

--- src/app/ver-mas/meseros/pantalla-t1b/page.js
+++ src/app/ver-mas/meseros/pantalla-t1b/page.js
@@ -3,7 +3,7 @@ import WppBtn from "@/app/components/WppBtn/WppBtn";
 import Footer from "@/app/components/Footer/Footer";
 import style from "../../../turnero-turnoexpress/components/RelatedProducts/TurnoExpressRelatedProducts.module.scss";
 import SecondaryHeader from "@/app/components/SecondaryHeader/SecondaryHeader";
-import ProductGallery from "../../turnos/pantalla-t1b/components/ProductGallery/ProductGallery";
+import ProductGallery from "./components/ProductGallery/ProductGallery";
 import LogosSection from "../../termicos/components/LogosSection/LogosSection";
 import TechnicalSheet from "./components/TechnicalSheet/TechnicalSheet";
 import HowItWorks from "./components/HowItWorks/HowItWorks";

5a57e11cfc214e421b57dcd3f96e5f753bedc622
SrPio <andresfer1799@gmail.com>
2025-03-10 14:26:33 -0500

    Adds metadata for SEO optimization in TurnoMaster landing page

--- src/app/turnomaster/page.js
+++ src/app/turnomaster/page.js
@@ -10,6 +10,46 @@ import TurnoMasterProductsSection from "./components/ProductsSection/TurnoMaster
 import TurnoMasterRelatedProducts from "./components/RelatedProducts/TurnoMasterRelatedProducts";
 import SecondaryTurnoMasterProductsSection from "./components/SecondaryProductsSection/SecondaryTurnoMasterProductsSection";
 
+export const metadata = {
+    title: "Sistema de Turnos: Agiliza Organiza las Filas en tu negocio",
+    description: "Sistema de turnos para organizar filas: Somos fabricantes. Ideal para agilizar la atención en salas de espera . ¡Solicita una asesoria ahora!",
+    viewport: "width=device-width, initial-scale=1, minimum-scale=1",
+    robots: "index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+    alternates: {
+        canonical: "https://landing-tecnologiaplus-nextjs.vercel.app/turnomaster",
+    },
+    openGraph: {
+        locale: "es_ES",
+        type: "article",
+        title: "Sistema de Turnos: Agiliza Organiza las Filas en tu negocio",
+        description: "Sistema de turnos para organizar filas: Somos fabricantes. Ideal para agilizar la atención en salas de espera . ¡Solicita una asesoria ahora!",
+        url: "https://tecnologiaplus.com/software-para-turnos/",
+        siteName: "TecnologiaPlus",
+        images: [
+            {
+                url: "https://tecnologiaplus.com/wp-content/uploads/2020/09/turnos1.png",
+                width: 900,
+                height: 514,
+                type: "image/png",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Sistema de Turnos: Agiliza Organiza las Filas en tu negocio",
+        description: "Sistema de turnos para organizar filas: Somos fabricantes. Ideal para agilizar la atención en salas de espera . ¡Solicita una asesoria ahora!",
+    },
+    icons: {
+        other: [
+            {
+                rel: "msapplication-TileImage",
+                url: "https://tecnologiaplus.com/wp-content/uploads/2018/08/cropped-favicon-tecnologia-plus-270x270.png",
+            },
+        ],
+    },
+};
+
+
 export default function TurnoMasterLanding() {
     return (
         <>

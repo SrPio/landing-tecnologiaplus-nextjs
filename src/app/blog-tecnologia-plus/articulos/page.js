84f6b786eb98241c4bed26a1e2e08e0cdb2aec1f
SrPio <andresfer1799@gmail.com>
2025-03-14 13:48:17 -0500

    Refactors import statements in BlogPostArticuloUno for improved clarity and consistency

--- src/app/blog-tecnologia-plus/articulos/page.js
+++ src/app/blog-tecnologia-plus/articulos/page.js
@@ -1,10 +1,9 @@
 import Header from "@/app/components/Header/Header";
 import altStyles from "../../components/Header/HeaderAlt.module.scss";
-const { default: ContactForm } = require("@/app/rollos-termicos/components/ContactForm/ContactForm");
-const { default: BlogHeroSection } = require("../components/HeroSection/BlogHeroSection");
-const { default: BlogPostEjemploUno } = require("./BlogPostEjemploUno");
-const { default: WppBtn } = require("@/app/components/WppBtn/WppBtn");
-const { default: Footer } = require("@/app/components/Footer/Footer");
+import BlogPostEjemploUno from "./BlogPostEjemploUno";
+import ContactForm from "@/app/components/ContactForm/ContactForm";
+import WppBtn from "@/app/components/WppBtn/WppBtn";
+import Footer from "@/app/components/Footer/Footer";
 
 export default function BlogPostArticuloUno() {
     return (

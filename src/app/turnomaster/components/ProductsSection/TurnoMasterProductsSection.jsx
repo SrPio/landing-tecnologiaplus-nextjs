f83f4e5ec02c224f4740954c826843412d6b4081
SrPio <andresfer1799@gmail.com>
2025-03-20 19:46:41 -0500

    Update link in TurnoMasterProductsSection for improved navigation

--- src/app/turnomaster/components/ProductsSection/TurnoMasterProductsSection.jsx
+++ src/app/turnomaster/components/ProductsSection/TurnoMasterProductsSection.jsx
@@ -4,7 +4,12 @@ function TurnoMasterProductsSection() {
   return (
     <section className={style.products__section}>
       <h2>Dos formas de solicitar el turno</h2>
-      <a className={style.card__btn} href="#">
+      <a
+        className={style.card__btn}
+        href="https://wa.me/573227347971"
+        target="_blank"
+        rel="noreferrer"
+      >
         ¡Quiero más información!
       </a>
       <div className={style.products__container}>

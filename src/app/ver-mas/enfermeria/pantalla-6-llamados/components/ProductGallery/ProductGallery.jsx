721a7db106fde19bfdbfe2a61570703e79df661c
SrPio <andresfer1799@gmail.com>
2025-03-20 21:26:58 -0500

    Add ProductGallery component for enhanced product display and navigation

---
+++ src/app/ver-mas/enfermeria/pantalla-6-llamados/components/ProductGallery/ProductGallery.jsx
@@ -0,0 +1,164 @@
+"use client";
+
+import { useState } from "react";
+import styles from "../../../../styles/ProductGallery.module.scss";
+import altStyles from "../../../../../components/Header/HeaderAlt.module.scss";
+import useDisableRightClick from "../../../../../../../hooks/useDisableRightClick";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { FaWhatsapp } from "react-icons/fa";
+import Header from "@/app/components/Header/Header";
+import Popup from "@/app/components/PopUp/Popup";
+
+function ProductGallery() {
+  const [isOpen, setIsOpen] = useState(false);
+  const images = [
+    "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1741481332/Frame_1_13_pdmf7w.webp",
+    "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1741481174/Frame_1_12_hhtcp2.webp",
+    "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1741481453/Frame_1_14_dlksjz.webp",
+    "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1741706821/Frame_1_d1yulx.webp",
+  ];
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const selectedImage = images[selectedIndex];
+
+  const handleNextImage = () => {
+    setSelectedIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrevImage = () => {
+    setSelectedIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : prevIndex
+    );
+  };
+
+  useDisableRightClick();
+
+  return (
+    <section className={styles.hero}>
+      <Header
+        customStyles={{
+          header: altStyles.altHeader,
+          phoneBtn: altStyles.altPhoneBtn,
+          searchIcon: altStyles.altSearchIcon,
+        }}
+      />
+
+      <div className={styles.action__nav}>
+        <a href="https://landing-tecnologiaplus-nextjs.vercel.app/enfermeria">
+          <IoIosArrowBack className={styles.icon__back} /> Volver
+        </a>
+      </div>
+
+      <div className={styles.container__product__gallery}>
+        <div className={styles.container__product__selectorImg}>
+          {/* Miniaturas */}
+          <div className={styles.container__thumbnails}>
+            {images.map((img, index) => (
+              <img
+                key={index}
+                src={img}
+                alt={`Thumbnail ${index + 1}`}
+                onClick={() => setSelectedIndex(index)}
+                className={selectedImage === img ? styles.active : ""}
+                style={{ cursor: "pointer" }}
+              />
+            ))}
+            <button
+              onClick={() => setIsOpen(true)}
+              className={styles.btn__multimedia}
+            >
+              Galería
+            </button>
+          </div>
+
+          {/* Imagen Principal */}
+          <div className={styles.container__product__selected}>
+            {selectedIndex > 0 && (
+              <IoIosArrowBack
+                className={styles.icon__prev}
+                onClick={handlePrevImage}
+                onMouseDown={(e) => e.preventDefault()}
+              />
+            )}
+            <img loading="lazy" src={selectedImage} alt="Imagen seleccionada" />
+            <IoIosArrowForward
+              className={styles.icon__next}
+              onClick={handleNextImage}
+              onMouseDown={(e) => e.preventDefault()}
+            />
+          </div>
+        </div>
+
+        {/* Información del producto */}
+        <div className={styles.container__info__products}>
+          <div className={styles.container__titles}>
+            <h1>Pantalla T6</h1>
+            <p>Para recibir llamados</p>
+          </div>
+
+          <div className={styles.container__contact__info}>
+            <a
+              href="https://wa.me/573227347971"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaWhatsapp className={styles.icon__wpp} />
+            </a>
+            <h3>¿Tienes preguntas?</h3>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat. Ut Wisi enim ad io
+          </p>
+        </div>
+      </div>
+
+      <Popup
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        tabs={[
+          {
+            key: "multimedia",
+            label: "Galería",
+            images: [
+              {
+                src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1741481332/Frame_1_13_pdmf7w.webp",
+                alt: "Imagen 1",
+              },
+              {
+                src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1741481174/Frame_1_12_hhtcp2.webp",
+                alt: "Imagen 2",
+              },
+              {
+                src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1741481453/Frame_1_14_dlksjz.webp",
+                alt: "Imagen 3",
+              },
+              {
+                src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1741706821/Frame_1_d1yulx.webp",
+                alt: "Imagen 4",
+              },
+              {
+                src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1740186399/1_4x-8_ehyify.webp",
+                alt: "Imagen 5",
+              },
+            ],
+          },
+          {
+            key: "medidas",
+            label: "Medidas",
+            images: [
+              {
+                src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1741706821/Frame_1_d1yulx.webp",
+                alt: "Imagen de medidas",
+              },
+            ],
+          },
+        ]}
+      />
+    </section>
+  );
+}
+
+export default ProductGallery;
--- src/app/ver-mas/enfermeria/pantalla-6-llamados/page.js
+++ src/app/ver-mas/enfermeria/pantalla-6-llamados/page.js
@@ -3,7 +3,7 @@ import WppBtn from "@/app/components/WppBtn/WppBtn";
 import Footer from "@/app/components/Footer/Footer";
 import style from "../../../enfermeria/components/RelatedProducts/EnfermeriaRelatedProducts.module.scss";
 import SecondaryHeader from "@/app/components/SecondaryHeader/SecondaryHeader";
-import ProductGallery from "@/app/ver-mas/meseros/pantalla-6-llamados/components/ProductGallery/ProductGallery";
+import ProductGallery from "./components/ProductGallery/ProductGallery";
 import LogosSection from "../../termicos/components/LogosSection/LogosSection";
 import TechnicalSheet from "@/app/ver-mas/meseros/pantalla-6-llamados/components/TechnicalSheet/TechnicalSheet";
 import HowItWorks from "./components/HowItWorks/HowItWorks";

fe46d3e89d94ef9cdf6f00d0bbc8c213e460147e
SrPio <andresfer1799@gmail.com>
2025-03-26 14:20:54 -0500

    Refines mobile responsiveness in HeroSection components; adjusts margins, adds mobile-specific text and images for improved layout and user experience.

--- src/app/turnomaster/components/HeroSection/TurnoMasterHeroSection.jsx
+++ src/app/turnomaster/components/HeroSection/TurnoMasterHeroSection.jsx
@@ -20,6 +20,12 @@ function TurnoMasterHeroSection({
           src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738860875/imagen_ppal_2x-8_1_cpkvb2.webp"
           alt="imagen de localizadores"
         />
+        <img
+          loading="lazy"
+          className={styles.hero__img__mobile}
+          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1743016309/3_4x-8_1_lidcip.webp"
+          alt="imagen de localizadores"
+        />
 
         <div className={styles.hero__text}>
           <h1>
@@ -27,6 +33,16 @@ function TurnoMasterHeroSection({
             <br />
             Sistema de turnos de espera
           </h1>
+          <p>Lleva tu sala de espera al siguiente nivel</p>
+
+          {boton ? (
+            <a className={styles.hero__btn} href={boton.href}>
+              {boton.text}
+            </a>
+          ) : null}
+        </div>
+        <div className={styles.hero__text__mobile}>
+          <h1>TurnoMaster: Sistema de turnos de espera</h1>
           <p>Lleva tu sala de espera al siguiente nivel</p>
 
           {boton ? (

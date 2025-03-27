b9cfd082deff6bd6b4235bd18ee708663212f89e
SrPio <andresfer1799@gmail.com>
2025-03-27 10:04:18 -0500

    Enhances video section layout and styling
    Refactors video container to include descriptive titles and a
    more structured layout for better readability and user experience.
    Updates SCSS to improve responsiveness and add consistent
    styling for video cards.
    
    No issue reference provided.

--- src/app/localizadores/components/SecondaryProductsSection/LocalizadoresSecondaryProductsSection.jsx
+++ src/app/localizadores/components/SecondaryProductsSection/LocalizadoresSecondaryProductsSection.jsx
@@ -58,28 +58,36 @@ function LocalizadoresSecondaryProductsSection() {
       <section className={`${style.products__section} ${style.video__section}`}>
         <div className={style.products__container}>
           <h2>Veamos funcionar nuestros Localizadores para Restaurantes</h2>
-          <div className={style.products__card__container}>
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/aB7FLAPCCJ8?si=COqYK2Ftu2pBov47"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
+          <div
+            className={`${style.products__card__container} ${style.video__container}`}
+          >
+            <div className={style.card__video}>
+              <p>Rec V3 - Localizadores para restaurantes</p>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/aB7FLAPCCJ8?si=COqYK2Ftu2pBov47"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </div>
 
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/b2nlu7sOlcI?si=4sszI32LRP3rQTlP"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
+            <div className={style.card__video}>
+              <p>Cir C2 - Localizadores para restaurantes</p>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/b2nlu7sOlcI?si=4sszI32LRP3rQTlP"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </div>
           </div>
         </div>
       </section>

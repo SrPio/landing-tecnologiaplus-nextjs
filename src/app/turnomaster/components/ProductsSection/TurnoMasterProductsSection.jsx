432e807c3bc46f24e37158e135d4257366cbe580
SrPio <andresfer1799@gmail.com>
2025-04-15 11:53:31 -0500

    Fix image source and update alt text for mobile view in TurnoMasterProductsSection component

--- src/app/turnomaster/components/ProductsSection/TurnoMasterProductsSection.jsx
+++ src/app/turnomaster/components/ProductsSection/TurnoMasterProductsSection.jsx
@@ -33,17 +33,27 @@ function TurnoMasterProductsSection() {
         >
           ¡Quiero más información!
         </a>
+
+        <img
+          loading="lazy"
+          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1742994791/8_4x-8_xto11r.webp"
+          alt="Como solicitar el turno desde casa"
+          className={style.img__mobile}
+        />
+        <a
+          className={style.card__btn__mobile}
+          href="https://wa.me/573227347971"
+          target="_blank"
+          rel="noreferrer"
+        >
+          ¡Quiero más información!
+        </a>
+
         <img
           loading="lazy"
           src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738860441/pedestal_2x-8_eurzvd.webp"
           alt="Como solicitar el turno de manera presencial"
           className={style.img__desktop}
-        />
-        <img
-          loading="lazy"
-          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1742994790/9_4x-8_zejb3t.webp"
-          alt="Como solicitar el turno de manera presencial"
-          className={style.img__mobile}
         />
         <img
           loading="lazy"

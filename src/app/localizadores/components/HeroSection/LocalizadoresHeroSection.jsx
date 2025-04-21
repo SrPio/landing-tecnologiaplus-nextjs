b8ff236a60d5df6ca8754b756b3d217549e09d74
SrPio <andresfer1799@gmail.com>
2025-04-21 15:10:36 -0500

    fix(LocalizadoresHeroSection): enhance mobile layout and text styles for improved readability and responsiveness

--- src/app/localizadores/components/HeroSection/LocalizadoresHeroSection.jsx
+++ src/app/localizadores/components/HeroSection/LocalizadoresHeroSection.jsx
@@ -7,8 +7,10 @@ function LocalizadoresHeroSection({
   backgroundTransparent = false,
   boton,
 }) {
-  const desktopImageUrl = "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1744781254/Imagen_portada_4x-8_dfrjjg_1_1_lvtv17.webp";
-  const mobileImageUrl = "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1744781156/Imagen_portada_4x-8_dfrjjg_1_cg4eew.webp";
+  const desktopImageUrl =
+    "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1744781254/Imagen_portada_4x-8_dfrjjg_1_1_lvtv17.webp";
+  const mobileImageUrl =
+    "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1745266173/Recurso_17_3x-8_1_sjgfak.webp";
 
   return (
     <section
@@ -34,7 +36,7 @@ function LocalizadoresHeroSection({
           alt="Localizadores para clientes"
           width={375}
           height={400}
-          loading="eager" 
+          loading="eager"
           fetchPriority="high"
           optimizeAfterHydration={false}
         />
@@ -58,13 +60,18 @@ function LocalizadoresHeroSection({
 
         <div className={styles.hero__text__mobile}>
           <h1>
-            Localizadores para restaurantes:
+            <span>Localizadores</span>
             <br />
+            para restaurantes
           </h1>
           <p>
-            Tus clientes disfrutan el tiempo
-            <br />
-            sin temor de perder el turno.
+            <span>
+              Tus clientes disfrutan el
+              <br />
+              tiempo sin temor de
+              <br />
+              perder eI turno.
+            </span>
           </p>
           {boton ? (
             <a className={styles.hero__btn} href={boton.href}>

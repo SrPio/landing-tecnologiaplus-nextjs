cdfb61b7e9383b9ff648cba976823064dec8fed7
SrPio <andresfer1799@gmail.com>
2025-04-22 12:56:33 -0500

    fix(LogosSection): update logo variable name for consistency in rendering

--- src/app/ver-mas/turnos/fila-unica/components/LogosSection/LogosSection.jsx
+++ src/app/ver-mas/turnos/fila-unica/components/LogosSection/LogosSection.jsx
@@ -5,16 +5,15 @@ import { useLogos } from "../../../../../context/LogosContext";
 import InfiniteSlider2 from "@/app/components/sliders/InfiniteSlider2";
 
 function LogosSection() {
-  const { logosTurnos } = useLogos();
-  
- 
+  const { logosTurnoExpress } = useLogos();
+
   return (
     <section className={styles.logossection__container}>
       <div className={styles.header}>
         <h2>Algunos clientes</h2>
       </div>
       <div className={styles.content}>
-        <InfiniteSlider2 logos={logosTurnos} speed={5000} />
+        <InfiniteSlider2 logos={logosTurnoExpress} speed={5000} />
       </div>
     </section>
   );

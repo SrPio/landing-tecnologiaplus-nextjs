ebe42007a9ffc7030efb14c1658bedb84d6285fe
SrPio <andresfer1799@gmail.com>
2025-04-22 13:28:16 -0500

    changed name folder rollos termicos page

--- src/app/ver-mas/termicos/page.js
+++ src/app/ver-mas/termicos/page.js
@@ -4,7 +4,7 @@ import ContactForm from "@/app/components/ContactForm/ContactForm";
 import WppBtn from "@/app/components/WppBtn/WppBtn";
 import Footer from "@/app/components/Footer/Footer";
 import style from "../../rollos-termicos/components/RelatedProducts/RelatedProducts.module.scss";
-import RelatedProducts from "@/app/rollos-termicos/components/RelatedProducts/RelatedProducts";
+import RelatedProducts from "@/app/papel-termico/components/RelatedProducts/RelatedProducts";
 import SecondaryHeader from "@/app/components/SecondaryHeader/SecondaryHeader";
 import ProductGallerySelected from "./components/ProductGallery/ProductGallerySelected";
 

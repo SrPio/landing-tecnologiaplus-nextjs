558fb892dd789d69ccdd5954c17c6cb55a36e483
SrPio <andresfer1799@gmail.com>
2025-04-25 20:00:06 -0500

    fix: update line breaks for better mobile responsiveness in benefits section

--- src/app/home/components/BenefitsSection/HomeBenefitsSection.jsx
+++ src/app/home/components/BenefitsSection/HomeBenefitsSection.jsx
@@ -23,8 +23,7 @@ function HomeBenefitsSection() {
             src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738944984/icono_1_2x-8_3_fvtagl.webp"
             alt="icono de auriculares"
           />
-          Asesoría según tu sector
-          <br />
+          Asesoría según tu sector <br className={style.hide__on__mobile} />
           para decisiones clave.
         </li>
         <li>
@@ -33,8 +32,8 @@ function HomeBenefitsSection() {
             src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738944983/icono_2_2x-8_3_zzggy6.webp"
             alt="icono número 10 con signo de +"
           />
-          Más de 10 años ayudando a las
-          <br />
+          Más de 10 años ayudando a las{" "}
+          <br className={style.hide__on__mobile} />
           empresas a mejorar el servicio.
         </li>
         <li>
@@ -43,8 +42,8 @@ function HomeBenefitsSection() {
             src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738944981/icono_3_2x-8_3_emxtsn.webp"
             alt="icono de mantenimiento"
           />
-          Soporte Técnico preventivo y correctivo
-          <br />
+          Soporte Técnico preventivo y correctivo{" "}
+          <br className={style.hide__on__mobile} />
           para proteger tu inversión.
         </li>
       </ul>
